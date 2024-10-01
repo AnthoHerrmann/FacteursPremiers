53eb13a91cc38b400b898c4299a5df4075d0b44b
Anthony Herrmann <aherrmann001@iutbayonne.univ-pau.fr>
2024-10-01 10:44:33 +0200

    Erreur volontaire du test

--- src/main/java/com/aherrmann/facteursPremiers/FacteursPremiers.java
+++ src/main/java/com/aherrmann/facteursPremiers/FacteursPremiers.java
@@ -1,4 +1,9 @@
 package com.aherrmann.facteursPremiers;
 
+import java.util.List;
+
 public class FacteursPremiers {
+    public static List<Integer> generer(int nombre) {
+        return null;
+    }
 }
--- src/test/java/com/aherrmann/facteursPremiers/FacteursPremiersTest.java
+++ src/test/java/com/aherrmann/facteursPremiers/FacteursPremiersTest.java
@@ -1,8 +1,26 @@
 package com.aherrmann.facteursPremiers;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.*;
 
 class FacteursPremiersTest {
 
+    @Test
+    void devrait_renvoyer_la_liste_des_diviseurs_premiers_de_ce_nombre() {
+        //GIVEN
+        FacteursPremiers facteurs = new FacteursPremiers();
+        int nombre = 2;
+        List<Integer> expected = List.of(2);
+
+        //WHEN
+        List<Integer> resultat = FacteursPremiers.generer(nombre);
+
+        //THEN
+        assertThat(resultat).isEqualTo(expected);
+    }
 }
