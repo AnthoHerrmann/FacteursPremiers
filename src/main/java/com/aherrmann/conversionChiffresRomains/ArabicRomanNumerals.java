1b3e23c88159eb25b9943b30597c3f4346717d9f
Anthony Herrmann <aherrmann001@iutbayonne.univ-pau.fr>
2024-10-02 10:41:03 +0200

    Test convert devrait renvoyer I pour la valeur 1

--- src/main/java/com/aherrmann/conversionChiffresRomains/ArabicRomanNumerals.java
+++ src/main/java/com/aherrmann/conversionChiffresRomains/ArabicRomanNumerals.java
@@ -2,6 +2,14 @@ package com.aherrmann.conversionChiffresRomains;
 
 public class ArabicRomanNumerals {
     public static String convert(Integer nombre) {
-        return "";
+        String resultat = "";
+
+        if (nombre != null) {
+            if (nombre == 1) {
+                resultat = "I";
+            }
+        }
+
+        return resultat;
     }
 }
--- src/test/java/com/aherrmann/conversionChiffresRomains/ArabicRomanNumeralsTest.java
+++ src/test/java/com/aherrmann/conversionChiffresRomains/ArabicRomanNumeralsTest.java
@@ -6,7 +6,7 @@ import static org.assertj.core.api.Assertions.*;
 class ArabicRomanNumeralsTest {
 
     @Test
-    void convert_devrait_renvoyer_null_pour_la_valeur_0() {
+    void convert_devrait_renvoyer_une_chaine_vide_pour_la_valeur_null() {
         //GIVEN
         Integer nombre = null;
 
@@ -15,5 +15,17 @@ class ArabicRomanNumeralsTest {
 
         //THEN
         assertThat(resultat).isEqualTo("");
+    }
+
+    @Test
+    void convert_devrait_renvoyer_I_pour_la_valeur_1() {
+        //GIVEN
+        Integer nombre = 1;
+
+        //WHEN
+        String resultat = ArabicRomanNumerals.convert(nombre);
+
+        //THEN
+        assertThat(resultat).isEqualTo("I");
     }
 }
